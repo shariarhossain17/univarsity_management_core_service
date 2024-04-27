f47d39e017e5899e6d9bd4775c628bb7bc3f69f8
shariarhossain17 <shariarhossain23@gmail.com>
2024-04-27 20:09:15 +0600

    server running

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,14 +1,13 @@
 import express from 'express';
 
 const router = express.Router();
-
+const route = '/route';
 const moduleRoutes = [
-  // ... routes
   {
-    path: '',
-    routes: '',
+    path: '/example',
+    routes: route,
   },
 ];
 
-moduleRoutes.forEach(route => router.use(route.path, route.routes));
+// moduleRoutes.forEach(route => router.use(route.path, route.routes));
 export default router;

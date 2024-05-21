1dc83e83f1b3b577c5b49a900fe35af34e419b90
shariarhossain17 <shariarhossain23@gmail.com>
2024-05-21 21:04:14 +0600

    added auth service

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,4 +8,8 @@ export default {
   env: process.env.NODE_ENV,
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
+  jwt: {
+    secret: process.env.JWT_SECRET,
+    expires_in: process.env.JWT_EXPIRES_IN,
+  },
 };
---
+++ src/middlewares/auth.ts
@@ -0,0 +1,31 @@
+import { NextFunction, Request, Response } from 'express';
+import { Secret } from 'jsonwebtoken';
+import config from '../config';
+import ApiError from '../errors/apiError';
+import { jwtHelpers } from '../helper/jwtHelper';
+
+const auth =
+  (...requiredRoles: string[]) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const token = req.headers.authorization;
+      if (!token) {
+        throw new ApiError(401, 'unauthorized access');
+      }
+
+      let verifiedUser = null;
+
+      verifiedUser = jwtHelpers.verifyToken(token, config.jwt.secret as Secret);
+
+      req.user = verifiedUser;
+
+      if (requiredRoles.length && !requiredRoles.includes(verifiedUser.role)) {
+        throw new ApiError(403, 'forbidden access');
+      }
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+
+export default auth;

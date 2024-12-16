256bc5b51977dc29ccbb12d24419ff4f3b1f0548
shariarhossain17 <shariarhossain23@gmail.com>
2024-12-17 00:51:21 +0600

    updated and created event

--- src/app/models/academic_semester/academic.semester.services.ts
+++ src/app/models/academic_semester/academic.semester.services.ts
@@ -1,17 +1,34 @@
 import { AcademicSemester, Prisma } from '@prisma/client';
+import ApiError from '../../../errors/apiError';
 import { paginationHelpers } from '../../../helper/pagination.helper';
 import { IGenericResponse } from '../../../interface/common';
 import { IPaginationOptions } from '../../../interface/pagination';
 import prisma from '../../../shared/prisma';
+import { RedisClient } from '../../../shared/redis';
 import { IAcademicSemesterFilters } from './academic.semeter.interface';
-import { academicSemesterFilterableField } from './academis.semester.constant';
+import {
+  EVENT_ACADEMIC_SEMESTER_CREATED,
+  EVENT_ACADEMIC_SEMESTER_UPDATED,
+  academicSemesterFilterableField,
+  validSemesterCode,
+} from './academis.semester.constant';
 
 const insertAcademicSemester = async (
   semesterData: AcademicSemester
 ): Promise<AcademicSemester> => {
+  if (semesterData.code !== validSemesterCode[semesterData.title]) {
+    throw new ApiError(400, 'semester code invalid');
+  }
   const result = await prisma.academicSemester.create({
     data: semesterData,
   });
+
+  if (result) {
+    await RedisClient.pubClient(
+      EVENT_ACADEMIC_SEMESTER_CREATED,
+      JSON.stringify(result)
+    );
+  }
 
   return result;
 };
@@ -28,7 +45,7 @@ const getAllAcademicSemester = async (
 
   if (searchTerm) {
     andConditions.push({
-      OR: academicSemesterFilterableField.map(field => ({
+      OR: academicSemesterFilterableField.map((field) => ({
         [field]: {
           contains: searchTerm,
           mode: 'insensitive',
@@ -39,7 +56,7 @@ const getAllAcademicSemester = async (
 
   if (Object.keys(filtersData).length > 0) {
     andConditions.push({
-      AND: Object.keys(filtersData).map(key => ({
+      AND: Object.keys(filtersData).map((key) => ({
         [key]: {
           equals: (filtersData as any)[key],
         },
@@ -97,6 +114,15 @@ const updateAcademicSemester = async (
     },
     data: payload,
   });
+
+  if (result) {
+    if (result) {
+      await RedisClient.pubClient(
+        EVENT_ACADEMIC_SEMESTER_UPDATED,
+        JSON.stringify(result)
+      );
+    }
+  }
 
   return result;
 };
--- src/app/models/academic_semester/academis.semester.constant.ts
+++ src/app/models/academic_semester/academis.semester.constant.ts
@@ -13,3 +13,12 @@ export const academicSemesterFilterableField = [
   'startMonth',
   'endMonth',
 ];
+
+export const validSemesterCode: { [key: string]: string } = {
+  Autumn: '01',
+  Summer: '02',
+  Fall: '03',
+};
+
+export const EVENT_ACADEMIC_SEMESTER_CREATED = 'academicSemester.created';
+export const EVENT_ACADEMIC_SEMESTER_UPDATED = 'academicSemester.updated';
